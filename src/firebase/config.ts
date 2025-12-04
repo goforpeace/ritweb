66b750bc8244ea278d829f01cab73561dbff8eae
Remotized IT <remotizedit@gmail.com>
2025-12-04 22:31:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-259280768-b6bf6",
+  "appId": "1:912879505304:web:dac155216b2059a35e65ed",
+  "apiKey": "AIzaSyA6lNlzUuk8oNRy2oNdfK-Z-0oRnsz_bU8",
+  "authDomain": "studio-259280768-b6bf6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "912879505304"
+};
